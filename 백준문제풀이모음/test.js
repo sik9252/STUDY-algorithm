0aacdbb8ffccb91b87d8395f887281fb87f76016
sik9252 <sik9252@naver.com>
2022-01-18 15:21:25 +0900

    문제풀이 기본 세팅

--- 백준문제풀이모음/test.js
+++ 백준문제풀이모음/test.js
@@ -1,3 +1,4 @@
+// 여기에 알고리즘 구현해서 테스트해볼것임
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split(" ");
